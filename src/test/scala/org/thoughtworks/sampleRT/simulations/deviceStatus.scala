ad0c950e60affd527e103252cf572ebdc0448dc5
DAS <das@dass-mbp.global.ds.honeywell.com>
2017-06-27 19:58:19 +0530

    externlized parameters

--- src/test/scala/org/thoughtworks/sampleRT/simulations/deviceStatus.scala
+++ src/test/scala/org/thoughtworks/sampleRT/simulations/deviceStatus.scala
@@ -1,49 +1,48 @@
 package  org.thoughtworks.sampleRT.simulations;
 
+
+
+
+
+import java.io.File
+
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import com.typesafe.config._
+
 
 
 
 class deviceStatus extends Simulation {
 
-
-  val headers_0 = Map(
-    "Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Upgrade-Insecure-Requests" -> "1")
-  val apimSubscriptionKey = Map("Ocp-Apim-Subscription-Key" -> "ea5a84106acb412a9d64d9e8c693962d")
-
-  //val apimSubscriptionKey = Map("Ocp-Apim-Subscription-Key" -> "823ed70bf7134f1db011c3a4f892dc64")
-  val headers_3 = Map("Pragma" -> "no-cache")
-  val headers_6 = Map("Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-  val uri1 = "https://granitedev.azure-api.net/dev-stub"
-  //val uri1 = "https://graniteqa.azure-api.net/qa-staging"
-  val userName = "debendra250@grr.la"
-  //val userName = "test1234@grr.la"
-  val password = "Password.1234"
+  val targetEnv = System.getProperty("targetEnv")
+  var file:File = new File(targetEnv+".conf")
+  val conf = ConfigFactory.parseFile(file)
+  val uri1 =conf.getString("BaseURL")
+  val userName = conf.getString("userName")
+  val password = conf.getString("password")
+  val subscriptionKey = conf.getString("apimSubscriptionKey")
+  val apimSubscriptionKey = Map("Ocp-Apim-Subscription-Key" -> subscriptionKey)
   var maybeaccessToken: String = ""
-  var prefix: String = "Bearer"
-  //val attribute: SessionAttribute = session("authToken")
-
-
   val contentType = Map("Content-Type" -> "application/json")
 
+
+
   val getAccessToken = scenario("Scenario 1")
-    .exec(http("DAS sign in")
+      .exec{session =>session.set("username1",userName)}
+      .exec{session =>session.set("password1",password)}
+      .exec(http("DAS sign in")
       .post(uri1 + "/signin")
       .headers(contentType)
       .headers(apimSubscriptionKey)
-      //.body(StringBody("{ 'username':" +userName+",password:"+password+"}")).asJSON
-      //.body(StringBody("""{ "username": "test1234@grr.la","password":"Password.1234" }""")).asJSON
-      .body(StringBody("""{ "username": "debendra250@grr.la","password":"Password.1234" }""")).asJSON
+      .body(StringBody("""{ "username": "${username1}","password":"${password1}" }""")).asJSON
       .check(status.is(200))
       .check(jsonPath("$.accessToken").saveAs("response1")))
-    .exec(session => session.set("authToken1", session.get("response1").as[String]))
+      .exec(session => session.set("authToken1", session.get("response1").as[String]))
+      .exec(scn2.deviceStatus)
 
-    .exec(ObjectB.bar)
-
-    object ObjectB {
-  val bar =group("Get Device status") {
+  object scn2 {
+  val deviceStatus =group("Get Device status") {
       val authToken = "Bearer " + "${authToken1}"
       exec(http("Get Device status")
       .get(uri1 + "/api/devices/00d02d9d12d7")
@@ -57,16 +56,11 @@ class deviceStatus extends Simulation {
 
 
 
-
-
   setUp(getAccessToken.inject(rampUsers(1) over (1)))
   	  .assertions(global.responseTime.max.lt(5000))
       .assertions(global.failedRequests.count.is(0))
       .assertions(forAll.responseTime.max.lt(3000))
 
-
-
-	  
 
 
 }
